49cd0e08e7486369674bd46d1f12d7a2a21d0086
BJ <almightybeeij@gmail.com>
2012-03-29 18:29:58 -0500

    Deleted non needed items and created a new directory called mobile for Android specific code

--- SDD2012Website/Scripts/myScript.js
+++ SDD2012Website/Scripts/myScript.js
@@ -8,24 +8,4 @@ pageRedirect( page)
 		header("Loacation: c.html");
 }
 
-var arr = ["../Images/FairOaksNight.jpg", "../Images/linux-background-wallpapers.jpeg", "../Images/linuxPengGreen.jpg", "../Images/UbuntuBlack.jpg"];
 
-(function recurse(counter) {
-    var pic = arr[counter];
-    $('#rightPane').delay('1200').animate({
-        background-image: image
-    }, 600);
-    delete arr[counter];
-    arr.push(color);
-    setTimeout(function() {
-        recurse(counter + 1);
-    }, 200);
-})(0);
-
-$(document).ready(function(){
-   
-   $('#rightPane').delay('9999').animate({
-	background-image : '../Images/UbuntuBlack.jpg'
-    },600);
-
-)}
