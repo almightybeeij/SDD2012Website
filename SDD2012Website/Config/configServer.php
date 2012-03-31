26f4814b350ab2b8f6594bf1f1696658fc28ca79
BJ <almightybeeij@gmail.com>
2012-03-31 14:06:25 -0500

    New Folder for Config. connection and databae php files are placed inside new Config Folder.

---
+++ SDD2012Website/Config/configServer.php
@@ -0,0 +1,9 @@
+<?php
+
+$server = 'instance9611.db.xeround.com:4974';
+$username = 'ljbc';
+$password = 'cdlwcdlw';
+$database = 'sdd2012pm';
+
+?>
+
---
+++ SDD2012Website/Config/connectServer.php
@@ -0,0 +1,7 @@
+<?php
+
+$conn = mysql_connect($server,$username,$password) or die ('Error connect to mysql');
+mysql_select_db($database);
+
+?>
+
