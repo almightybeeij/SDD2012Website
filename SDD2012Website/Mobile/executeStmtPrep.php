8ce5dbdf8a797e1d0c6283ac4ddd2381f292ab8a
Cory Wilson <wilson7880@gmail.com>
2012-04-02 16:09:58 -0500

    modify

--- SDD2012Website/Mobile/executeStmtPrep.php
+++ SDD2012Website/Mobile/executeStmtPrep.php
@@ -14,15 +14,14 @@ $stmt = $conn_mysqli->prepare("SELECT Boundary1 FROM parkinglot WHERE lotid = (?
 $stmt->bind_param("i", $bind_id);
 $stmt->execute();
 
-$out_boundary = NULL;
-$stmt->bind_result($out_boundary);
+//$out_boundary = NULL;
+//$stmt->bind_result($out_boundary);
 
 $md = $stmt->result_metadata();
 $output = array();
-
 $output[] = $md->fetch_assoc();
 
-print(json_encode($md));
+print(json_encode($output));
 
 include '../Config/closedbServerI.php';
 
