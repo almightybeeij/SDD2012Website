5527c2159d083e08b8d0ef884f5ec235f13169e5
Cory Wilson <wilson7880@gmail.com>
2012-04-02 14:00:52 -0500

    Added close script for mysqli

--- SDD2012Website/Config/connectServerI.php
+++ SDD2012Website/Config/connectServerI.php
@@ -4,8 +4,6 @@ include 'configServer.php';
 
 $conn_mysqli = new mysqli($host, $username, $password, $database, $port);
 
-echo $host;
-
 if ($conn_mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
