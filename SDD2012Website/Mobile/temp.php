3d7290c3c4104f5067001babab616b7b22ca7a07
Cory Wilson <wilson7880@gmail.com>
2012-04-02 14:23:05 -0500

    test prepared statment

--- SDD2012Website/Mobile/temp.php
+++ SDD2012Website/Mobile/temp.php
@@ -4,11 +4,21 @@
 session_start();
 include '../Config/connectServerI.php';
 
-echo $conn_mysqli->host_info . "\n";
+echo $conn_mysqli->host_info . "\n\n";
 
+$id = 18;
+$stmt = $conn_mysqli->prepare("SELECT Boundary1 FROM parkinglot WHERE lotid = (?)");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+
+$out_boundary = NULL;
+$stmt->bind_result($out_boundary);
+
+while ($stmt->fetch()) {
+	echo $out_boundary;
+}
+	
 include '../Config/closedbServerI.php';
-
-echo $conn_mysqli->host_info . "\n No Connection";
 
 ?>
 </html>
