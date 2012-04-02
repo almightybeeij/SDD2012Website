5c05c966057d2276ad2e0ea181edae6bc94daf62
Cory Wilson <wilson7880@gmail.com>
2012-04-02 13:48:39 -0500

    testing connection

--- SDD2012Website/Config/connectServerI.php
+++ SDD2012Website/Config/connectServerI.php
@@ -4,6 +4,8 @@ include 'configServer.php';
 
 $conn_mysqli = new mysqli($host, $username, $password, $database, $port);
 
+echo $host;
+
 if ($conn_mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
