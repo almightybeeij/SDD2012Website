4c8c59220b03e6782d54629c43b582a59844c049
Cory Wilson <wilson7880@gmail.com>
2012-04-02 15:08:17 -0500

    modify executeStmtPrep.php

--- SDD2012Website/Mobile/executeStmtPrep.php
+++ SDD2012Website/Mobile/executeStmtPrep.php
@@ -3,9 +3,22 @@
 session_start();
 include '../Config/connectServerI.php';
 
-foreach ($_GET as $key => $value)
-{
-	print "$key has a value of $value";
+//foreach ($_GET as $key => $value)
+//{
+//	print "$key has a value of $value";
+//}
+
+$bind_id = $_GET["lotid"];
+
+$stmt = $conn_mysqli->prepare("SELECT Boundary1 FROM parkinglot WHERE lotid = (?)");
+$stmt->bind_param("i", $bind_id);
+$stmt->execute();
+
+$out_boundary = NULL;
+$stmt->bind_result($out_boundary);
+
+while ($stmt->fetch()) {
+	echo $out_boundary;
 }
 
 include '../Config/closedbServerI.php';
