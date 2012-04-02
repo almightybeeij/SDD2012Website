66586ba98c183a9595dc7e9c66ad1a854438d2ff
BJ <almightybeeij@gmail.com>
2012-04-02 13:09:31 -0500

    Added ability to change password in update page with error checking. Next is task is to make the forgot password page email new password.

--- SDD2012Website/updateUser.php
+++ SDD2012Website/updateUser.php
@@ -33,10 +33,32 @@
 
 			<!-- Main Content Area-->
 			<div id="content">
-				Here is your user information<br>
+				Here is your user information<br><br>
 
 				<?php
-
+				$passwordUpdateError = null;
+				if (isset ($_POST['passwordUpdate']))
+				{
+					if($_POST['password1TextBox'] == $_POST['password2TextBox'])
+					{
+						if(empty($_POST['password1TextBox']))
+							$passwordUpdateError = "Password fields must not be blank";
+						else 
+						{
+							$sha256Pass = hash ( 'sha256' , $_POST['password1TextBox'] );
+							$sqlUpdate = "update client set password='$sha256Pass' where email='$_SESSION[email]';";
+							$result = mysql_query($sqlUpdate);
+							
+							if (!$result)
+								die('Invalid query: ' . mysql_error());
+							
+							$_SESSION['password']=$sha256Pass;
+							
+						}
+					}
+					else 
+						$passwordUpdateError = "Your passwords did not match";
+				}
 
 				/* This part of code is for when the user updates their fields in the text boxes
 				 * It also updates both the client and clientsession tables to reflect the changes
@@ -101,7 +123,42 @@
 						echo "</tr>";
 					}
 				}
-
+				else if (isset ($_POST['passwordButton']) || !empty($passwordUpdateError))
+				{
+				?>
+					<form name='update' action='<?=$PHP_SELF?>' method='post'>
+						<table>
+						<tr>
+							<th>Enter Password</th><th>Reenter Password</th><th>Update Password?</th>
+						</tr>
+						<tr>
+							<td><input id='password' type='text' name='password1TextBox' /></td>
+							<td><input id='lastName' type='text' name='password2TextBox' /></td>
+							<td><input id='passwordUpdate' type='submit' name='passwordUpdate' value='Update'/></td>
+						</tr>
+						<?php 
+							if ($passwordUpdateError == "Your passwords did not match")
+							{
+								echo "<tr>";
+								echo "<td style='border:0px'></td>";
+								echo "<td style='color:red; font-weight:bold; border:0px'>$passwordUpdateError</td>";
+								echo "</tr>";
+								$passwordUpdateError = null;
+							}
+							else if ($passwordUpdateError == "Password fields must not be blank")
+							{
+								echo "<tr>";
+								echo "<td style='border:0px'></td>";
+								echo "<td style='color:red; font-weight:bold; border:0px'>$passwordUpdateError</td>";
+								echo "</tr>";
+								$passwordUpdateError = null;
+							}
+						?>
+						</table>
+					</form>
+				<?php 	
+				}
+			
 				/* This part of the code is reached when user first comes to page because neither the edit or update is in $_POST array
 				 * This pulls the users information from the client table and displays them inside a table
 				*/
@@ -117,7 +174,7 @@
 						die('Invalid query: ' . mysql_error());
 
 					echo"<table id='preEditTable'>";
-					echo"<tr><th>First Name</th><th>Last Name</th><th>Email</th><th>Type</th><th>Edit Info?</th></tr>";
+					echo"<tr><th>First Name</th><th>Last Name</th><th>Email</th><th>Type</th><th>Edit Info?</th><th>Edit Password?</th></tr>";
 					while ($row = mysql_fetch_array($result))
 					{
 						echo "<tr>";
@@ -131,7 +188,9 @@
 							echo "<td>You are a Student</td>";
 							
 						echo "<td><form id='editForm' name='edit' action='$PHP_SELF?>' method='post'>";
-						echo "<input id='edit' type='submit' name='edit' value='Edit'/></td>";
+						echo "<input id='edit' type='submit' name='edit' value='Edit'/></form></td>";
+						echo "<td><form id='passwordForm' name='passwordUpdate' action='$PHP_SELF?>' method='post'>";
+						echo "<input id='password' type='submit' name='passwordButton' value='Yes'/></form></td>";
 						echo "</tr>";
 					}
 
@@ -140,7 +199,7 @@
 					
 				include "Config/closedbServer.php";
 				?>
-
+			
 			</div>
 
 			<!-- Right Side Pane-->
