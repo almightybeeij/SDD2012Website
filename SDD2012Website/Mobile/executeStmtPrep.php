fa31d8b15ed87458b8b767c4484f84014aaca9f0
Cory Wilson <wilson7880@gmail.com>
2012-04-03 14:40:02 -0500

    cwilson

--- SDD2012Website/Mobile/executeStmtPrep.php
+++ SDD2012Website/Mobile/executeStmtPrep.php
@@ -3,33 +3,8 @@
 session_start();
 
 include '../Config/connectServerI.php';
+include '../Config/dbUtility.php';
 
-function fetchArray (&$statement)
-{
-	$data = mysqli_stmt_result_metadata($statement);
-	$fields = array();
-	$out = array();
-
-	$fields[0] = $statement;
-	$count = 1;
-
-	while($field = mysqli_fetch_field($data))
-	{
-		$fields[$count] = &$out[$field->name];
-		$count++;
-	}
-
-	call_user_func_array(mysqli_stmt_bind_result, $fields);
-	//mysqli_stmt_fetch($statement);
-	return (count($out) == 0) ? false : $out;
-}
-
-//foreach ($_GET as $key => $value)
-//{
-//	print "$key has a value of $value";
-//}
-
-$eor = false;
 $bind_id = $_REQUEST["lotid"];
 $bind_avail = $_REQUEST["available"];
 
@@ -45,21 +20,6 @@ while ($stmt->fetch())
 {
 	print(json_encode($output));
 }
-//$stmt->store_result();
-
-//$meta = $stmt->result_metadata();
-//while ($columnName = $meta->fetch_field())
-//{
-//	$columns[] = &$results[$columnName->name];
-//}
-
-//while (!$eor)
-//{
-//	call_user_func_array(array($stmt, 'bind_result'), $columns);
-//	
-//	if ($stmt->fetch()) { $output[] = $results;	}
-//	else { $eor = true; }
-//}
 
 include '../Config/closedbServerI.php';
 
