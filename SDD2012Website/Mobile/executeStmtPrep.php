9cffbd4c985d41f2011cec6a263697f31195b518
Cory Wilson <wilson7880@gmail.com>
2012-04-03 00:49:42 -0500

    cwilson modify

--- SDD2012Website/Mobile/executeStmtPrep.php
+++ SDD2012Website/Mobile/executeStmtPrep.php
@@ -1,6 +1,7 @@
 <?php
 
 session_start();
+
 include '../Config/connectServerI.php';
 
 //foreach ($_GET as $key => $value)
@@ -8,15 +9,16 @@ include '../Config/connectServerI.php';
 //	print "$key has a value of $value";
 //}
 
-//echo $conn_mysqli->host_info . "<br/><br/>";
-
 $bind_id = $_REQUEST["lotid"];
 
 $stmt = $conn_mysqli->prepare("SELECT Boundary1 FROM parkinglot WHERE lotid = ?");
+
 $stmt->bind_param("i", $bind_id);
 $stmt->execute();
 $stmt->store_result();
+
 $meta = $stmt->result_metadata();
+
 while ($columnName = $meta->fetch_field()) {
 	$columns[] = &$results[$columnName->name];
 }
@@ -27,19 +29,6 @@ $stmt->fetch();
 $output[] = $results;
 
 print(json_encode($output));
-
-//$out_boundary = NULL;
-//$stmt->bind_result($out_boundary);
-
-//$md = $stmt->result_metadata();
-//$output = array();
-//$output[] = $md->fetch_assoc();
-
-//print(json_encode($output));
-
-//while ($stmt->fetch()) {
-//	$output[0] = array($out_boundary);
-//}
 
 include '../Config/closedbServerI.php';
 
