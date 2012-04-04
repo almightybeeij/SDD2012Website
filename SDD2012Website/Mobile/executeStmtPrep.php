680d8be74c4aa08269491208052c98ea1a11ba73
Cory Wilson <wilson7880@gmail.com>
2012-04-03 23:31:50 -0500

    cwilson

--- SDD2012Website/Mobile/executeStmtPrep.php
+++ SDD2012Website/Mobile/executeStmtPrep.php
@@ -12,27 +12,12 @@ foreach ($_REQUEST as $key => $value)
 	$vars[] = $value;	
 }
 
-$bind_id = $_REQUEST["lotid"];
-$bind_avail = $_REQUEST["available"];
+$sql = array_shift($vars);
 
-$sql = "SELECT * FROM parkingspace WHERE parkinglot_lotid = ? and available = ?";
+//$sql = "SELECT * FROM parkingspace WHERE parkinglot_lotid = ? and available = ?";
 $stmt = $conn_mysqli->prepare($sql);
 
 bindParameters($stmt, $vars);
-
-// $bindParamsMethod = new ReflectionMethod('mysqli_stmt', 'bind_param');
-// $bindParamsReferences = array();
-// $typeDefinitionString = array_shift($vars);
-// foreach($vars as $key => $value){
-// 	$bindParamsReferences[$key] = &$vars[$key];
-// }
-
-// array_unshift($bindParamsReferences,$typeDefinitionString);
-// $bindParamsMethod->invokeArgs($stmt,$bindParamsReferences);
-
-// call_user_func_array(mysqli_stmt_bind_param, $vars);
-
-// $stmt->bind_param("ii", $bind_id, $bind_avail);
 
 $stmt->execute();
 
