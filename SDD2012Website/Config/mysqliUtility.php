8ec99cb750afe942f5f16f8739554745aaee3f38
Cory Wilson <wilson7880@gmail.com>
2012-04-03 23:25:58 -0500

    cwilson modified bindParameters

---
+++ SDD2012Website/Config/mysqliUtility.php
@@ -0,0 +1,38 @@
+<?php
+
+function fetchArray (&$statement)
+{
+	$data = mysqli_stmt_result_metadata($statement);
+	$fields = array();
+	$out = array();
+
+	$fields[0] = $statement;
+	$count = 1;
+
+	while($field = mysqli_fetch_field($data))
+	{
+		$fields[$count] = &$out[$field->name];
+		$count++;
+	}
+
+	call_user_func_array(mysqli_stmt_bind_result, $fields);
+	
+	return (count($out) == 0) ? false : $out;
+}
+
+function bindParameters (&$stmt, &$bindParams)
+{
+	$bindParamsReferences = array();
+	$bindParamsMethod = new ReflectionMethod('mysqli_stmt', 'bind_param');
+		
+	$typeDefinitionString = array_shift($bindParams);
+	
+	foreach($bindParams as $key => $value) {
+		$bindParamsReferences[$key] = &$bindParams[$key];
+	}
+
+	array_unshift($bindParamsReferences,$typeDefinitionString);
+	$bindParamsMethod->invokeArgs($stmt,$bindParamsReferences);
+}
+
+?>
